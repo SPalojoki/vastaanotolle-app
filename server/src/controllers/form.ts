847a2d8afa39a798a428ccd123edab9829203e2b
Sakari Palojoki <palojoki.sakari@gmail.com>
2024-04-27 13:53:32 +0300

    Create endpoint for fetching form using access code

---
+++ server/src/controllers/form.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import prisma from '../db/prismaClient'
+
+const formRouter = express.Router()
+
+formRouter.get('/:accessCode', async (req, res) => {
+	const form = await prisma.form.findUnique({
+		where: {
+			accessCode: req.params.accessCode,
+		},
+		include: {
+			questions: true,
+		},
+	})
+
+	if (!form) {
+		return res.status(404).json({ error: 'Form not found!' })
+	}
+
+	return res.status(200).json(form)
+})
+
+export default formRouter
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors'
 
 import config from './utils/config'
 
+import formRouter from './controllers/form'
+
 const app = express()
 
 app.use(cors())
@@ -11,6 +13,8 @@ app.use(express.json())
 app.get('/', (_req, res) => {
 	res.send('Hello World!')
 })
+
+app.use('/form', formRouter)
 
 app.listen(config.port, () => {
 	console.log(`Server running on http://localhost:${config.port}`)
