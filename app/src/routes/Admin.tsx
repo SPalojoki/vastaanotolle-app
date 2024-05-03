cf986b7ea74fdfa609cfa7a7f736df834957db9e
Sakari Palojoki <palojoki.sakari@gmail.com>
2024-05-03 17:06:31 +0300

    Recreate admin forms view

--- app/src/routes/Admin.tsx
+++ app/src/routes/Admin.tsx
@@ -1,50 +1,70 @@
-import { HiDocumentText, HiUserCircle, HiOutlineLogout } from 'react-icons/hi'
-import { Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
-const SidebarButton = ({
-  text,
-  isActive,
+const NavigationButton = ({
+  to,
+  end,
   children,
 }: {
-  text: string
-  isActive: boolean
+  to: string
+  end: boolean | undefined
   children: React.ReactNode
 }) => {
   return (
-    <button
-      type='button'
-      className={`flex w-full items-center gap-3 rounded-md px-3 py-2 transition-all ${isActive ? 'scale-[1.01] bg-blue-800 font-bold text-white drop-shadow-md' : 'outline outline-1 outline-gray-200 hover:outline-gray-300'}`}
-    >
-      <div className='text-2xl'>{children}</div>
-      {text}
-    </button>
+    <li>
+      <NavLink
+        to={to}
+        end={end || false} // Determines whether the link is an exact match
+        className={({ isActive }) =>
+          isActive
+            ? 'rounded bg-gray-900 px-4 py-2 text-gray-200'
+            : 'rounded px-4 py-2 text-gray-300 transition-all hover:bg-gray-700'
+        }
+      >
+        {children}
+      </NavLink>
+    </li>
+  )
+}
+
+const NavigationBar = () => {
+  return (
+    <nav>
+      <div className='bg-gray-800 py-2'>
+        <div className='container mx-auto flex'>
+          <div className='flex items-center pr-12 text-gray-400'>
+            <p className='border-r-2 border-r-gray-200 pr-4 text-2xl font-bold '>
+              Vastaanotolle.fi
+            </p>
+            <h1 className=' pl-4 text-xl font-medium '>Admin tools</h1>
+          </div>
+          <ul className='flex items-center gap-2'>
+            <NavigationButton to={'/admin/'} end>
+              Forms
+            </NavigationButton>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+const Header = () => {
+  return (
+    <div className='z-10 shadow-md'>
+      <h2 className='container mx-auto my-6 text-4xl font-bold'>Forms</h2>
+    </div>
   )
 }
 
 const Admin = () => {
   return (
-    <div className='grid h-screen grid-cols-[300px_auto] text-zinc-800'>
-      <nav className='flex h-full flex-col items-start bg-slate-100 p-4'>
-        <div className='text-blue-900'>
-          <p className='text-sm font-semibold '>Vastaanotolle.fi</p>
-          <h1 className='mb-6 text-2xl font-bold'>Admin interface</h1>
+    <div className='flex h-screen flex-col'>
+      <NavigationBar />
+      <Header />
+      <main className='flex-grow bg-gray-100'>
+        <div className='container mx-auto py-10'>
+          <Outlet />
         </div>
-        <div className='flex w-full flex-col gap-3'>
-          <SidebarButton text='Forms' isActive={true}>
-            <HiDocumentText />
-          </SidebarButton>
-          <SidebarButton text='Manage account' isActive={false}>
-            <HiUserCircle />
-          </SidebarButton>
-        </div>
-        <div className='flex-grow' />
-        <button type='button' className='flex gap-2 text-red-500'>
-          <HiOutlineLogout size={24} className='rotate-180' />
-          Sign out
-        </button>
-      </nav>
-      <main className='bg-zinc-50 p-5'>
-        <Outlet />
       </main>
     </div>
   )
--- app/src/routes/Forms.tsx
+++ app/src/routes/Forms.tsx
@@ -19,46 +19,49 @@ const Forms = () => {
 
   return (
     <div className='w-full'>
-      <div className='mb-8 flex items-center justify-between'>
-        <h1 className=' text-3xl font-bold'>Forms</h1>
+      {data.length === 0 ? (
+        <p className='text-center font-light'>No forms created yet.</p>
+      ) : (
+        <table className='text-md overflow w-full overflow-hidden rounded-lg text-left text-gray-500 shadow-md'>
+          <thead className='overflow-hidden bg-gray-200 text-sm uppercase text-gray-700'>
+            <tr>
+              <th className='px-6 py-3'>Title</th>
+              <th className='px-6 py-3'>Access code</th>
+              <th className='px-6 py-3'>Published</th>
+              <th className='px-6 py-3'>Created at</th>
+              <th className='px-6 py-3'>Updated at</th>
+              <th className='px-6 py-3'>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((form: Form) => (
+              <tr key={form.id}>
+                <th className='px-6 py-3 font-medium text-gray-900'>
+                  {form.title}
+                </th>
+                <td className='px-6 py-3'>{form.accessCode}</td>
+                <td className='px-6 py-3'>{form.published ? 'Yes' : 'No'}</td>
+                <td className='px-6 py-3'>
+                  {format(new Date(form.createdAt), 'dd MMM yyyy HH:mm')}
+                </td>
+                <td className='px-6 py-3'>
+                  {format(new Date(form.updatedAt), 'dd MMM yyyy HH:mm')}
+                </td>
+                <td className='px-6 py-3 text-blue-600'>Edit</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      <div className='flex justify-center pt-8'>
         <button
           type='button'
-          className='flex items-center gap-2 rounded-md bg-blue-600 px-3 py-2 text-white transition-all hover:bg-blue-700'
+          className='flex items-center gap-2 rounded-md bg-blue-600 px-3 py-2 text-white shadow-md transition-all hover:bg-blue-700 hover:shadow-inner'
         >
           <HiPlus size={24} />
           Create new form
         </button>
       </div>
-      <table className='text-md w-full overflow-clip rounded-lg text-left text-gray-500 shadow-sm'>
-        <thead className='overflow-hidden bg-gray-200 text-sm uppercase text-gray-700'>
-          <tr>
-            <th className='px-6 py-3'>Title</th>
-            <th className='px-6 py-3'>Access code</th>
-            <th className='px-6 py-3'>Published</th>
-            <th className='px-6 py-3'>Created at</th>
-            <th className='px-6 py-3'>Updated at</th>
-            <th className='px-6 py-3'>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((form: Form) => (
-            <tr key={form.id}>
-              <th className='px-6 py-3 font-medium text-gray-900'>
-                {form.title}
-              </th>
-              <td className='px-6 py-3'>{form.accessCode}</td>
-              <td className='px-6 py-3'>{form.published ? 'Yes' : 'No'}</td>
-              <td className='px-6 py-3'>
-                {format(new Date(form.createdAt), 'dd MMM yyyy HH:mm')}
-              </td>
-              <td className='px-6 py-3'>
-                {format(new Date(form.updatedAt), 'dd MMM yyyy HH:mm')}
-              </td>
-              <td className='px-6 py-3 text-blue-600'>Edit</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </div>
   )
 }
