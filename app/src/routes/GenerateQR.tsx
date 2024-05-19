f5b180e202477a9a96b42324358929490c1f803c
Sakari Palojoki <palojoki.sakari@gmail.com>
2024-05-19 22:18:06 +0300

    Implement basic QR code functionality

--- app/src/routes/GenerateQR.tsx
+++ app/src/routes/GenerateQR.tsx
@@ -1,12 +1,26 @@
 import { useLocation } from 'react-router-dom'
+import QRCode from 'react-qr-code'
 
 const GenerateQR = () => {
   const location = useLocation()
   const encoded = new URLSearchParams(location.search).get('data')
   return (
-    <div>
-      Generate QR
-      <div>{encoded}</div>
+    <div className='grid h-full grid-rows-[2fr_4fr_3fr] items-center text-center'>
+      <div className='flex flex-col gap-4'>
+        <p> You are all set! </p>
+        <h1 className='text-2xl font-bold'>Here is your QR code</h1>
+      </div>
+      <div className='flex items-center justify-center'>
+        {encoded ? (
+          <div className='rounded-lg bg-white p-4'>
+            <QRCode value={encoded} />
+          </div>
+        ) : (
+          <p className='font-semibold text-red-500'>
+            Payload missing. Please resubmit the form.
+          </p>
+        )}
+      </div>
     </div>
   )
 }
